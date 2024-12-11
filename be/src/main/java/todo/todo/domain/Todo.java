450ae0283ad9f00deb73750b26ad9d4b4c34c4af
yuan <whyuan@gmail.com>
2024-12-12 04:59:20 +0900

    feat: api 확인용 코드 작성

---
+++ be/src/main/java/todo/todo/domain/Todo.java
@@ -0,0 +1,22 @@
+package todo.todo.domain;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity(name="Todo") // 투두 엔티티 등록
+@Table(name="todo") // 투두 테이블 생성
+@NoArgsConstructor // lombok: 생성자 생략
+@Getter // lombok: 게터 생성
+@Setter // lombok: 세터 생성
+
+public class Todo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    private String todo; //투두 필드
+}
---
+++ be/src/main/java/todo/todo/repository/TodoRepository.java
@@ -0,0 +1,9 @@
+package todo.todo.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import todo.todo.domain.Todo;
+
+@Repository
+public interface TodoRepository extends JpaRepository<Todo,Long> {
+}
