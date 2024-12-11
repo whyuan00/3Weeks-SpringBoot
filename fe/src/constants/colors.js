eedd03556ff2955174cc73f676234b1be7b7c13e
yuan <whyuan@gmail.com>
2024-12-12 04:09:55 +0900

    feat: color 추가

---
+++ fe/src/constants/colors.js
@@ -0,0 +1,12 @@
+const Colors = {
+  WHITE: "#FFFFFF",
+  BLACK: "#000000",
+  DARK_BLUE: "#37AFE1",
+  SKY_BLUE: "#4CC9FE",
+  GRAY: "#364F6B",
+  GRAY_GREEN: "#3FC1C9",
+  BEIGE: "#F5F5F5",
+  PINK_RED: "#FC5185",
+};
+
+export default Colors;
