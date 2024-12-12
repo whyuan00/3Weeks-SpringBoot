db09a2b507095f91841f1eaed0dc6e6ffee72480
yuan <whyuan@gmail.com>
2024-12-12 14:59:19 +0900

    style: 전역 배경 베이지 설정

--- fe/tailwind.config.js
+++ fe/tailwind.config.js
@@ -1,6 +1,24 @@
+const {
+  DARK_BLUE,
+  SKY_BLUE,
+  GRAY,
+  GRAY_GREEN,
+  BEIGE,
+  PINK_RED,
+} = require("./src/constants/colors"); // colors.js에서 BEIGE를 가져옵니다.
+
 /** @type {import('tailwindcss').Config} */
 export const content = ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"];
 export const theme = {
-  extend: {},
+  extend: {
+    colors: {
+      darkBlue: DARK_BLUE,
+      skyBlue: SKY_BLUE,
+      gray: GRAY,
+      grayGreen: GRAY_GREEN,
+      beige: BEIGE,
+      pinkRed: PINK_RED,
+    },
+  },
 };
 export const plugins = [];
