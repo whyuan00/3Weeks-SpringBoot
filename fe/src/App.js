8c4388d880b576b449f766aead65f81cc39abf45
yuan <whyuan@gmail.com>
2024-12-12 13:50:34 +0900

    chore: 기타 설정 추가가

--- fe/src/App.js
+++ fe/src/App.js
@@ -15,7 +15,6 @@ function App() {
       .get("/todos/")
       .then((res) => {
         setTodos(res.data);
-        console.log(res.data);
       })
       .catch((e) => {
         console.error("todo조회 에러: ", e);
